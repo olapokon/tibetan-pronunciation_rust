7ce1ea2f372c6e33eb4360654838a3224e9aa23b
ak4ra <aryjna1@gmail.com>
2021-01-30 18:31:18 +0200

    Add tests

--- tests/tibetan_pronunciation.rs
+++ tests/tibetan_pronunciation.rs
@@ -1,6 +1,40 @@
 use tibetan_pronunciation_rust::{*, tibetan_data::TibetanSyllable};
 
 #[test]
+fn with_subscript_ra() {
+    let roots = generate_tibetan_character_map();
+
+    let syllable = TibetanSyllable {
+        root: roots.get(&'ཏ').unwrap(),
+        prefix: None,
+        superscript: None,
+        subscript: Some(roots.get(&'ར').unwrap()),
+        suffix: None,
+        second_suffix: None,
+    };
+
+    assert_eq!("ཏྲ", tibetan(&syllable));
+    assert_eq!("trá", phonetic(&syllable));
+}
+
+#[test]
+fn with_subscript_ya() {
+    let roots = generate_tibetan_character_map();
+
+    let syllable = TibetanSyllable {
+        root: roots.get(&'ག').unwrap(),
+        prefix: None,
+        superscript: None,
+        subscript: Some(roots.get(&'ཡ').unwrap()),
+        suffix: None,
+        second_suffix: None,
+    };
+
+    assert_eq!("གྱ", tibetan(&syllable));
+    assert_eq!("khya", phonetic(&syllable));
+}
+
+#[test]
 fn root_change_with_diairesis() {
     let roots = generate_tibetan_character_map();
 
